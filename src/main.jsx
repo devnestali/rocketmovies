b08f3ac92cdce513dcd3da9f499cc5c639cf26eb
Victor Henrique de Camargo Nestali <victornestali@gmail.com>
2024-07-09 12:28:17 +0200

    Creating global styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { MoviePreview } from './pages/MoviePreview'
 import { ThemeProvider } from 'styled-components';
 import theme from './styles/theme';
+import GlobalStyle from './styles/global'; 
+
+import { MoviePreview } from './pages/MoviePreview'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
+      <GlobalStyle />
       <MoviePreview />
     </ThemeProvider>
   </React.StrictMode>,
--- src/pages/MoviePreview/index.jsx
+++ src/pages/MoviePreview/index.jsx
@@ -5,6 +5,7 @@ export function MoviePreview() {
     <Container>
       <h1>Hello World</h1>  
       <span>Victor Nestali</span>
+      <button>Pressione aqui</button>
     </Container>
   )
 }
