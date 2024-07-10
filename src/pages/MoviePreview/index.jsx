ae326dd95cf0f6e0a975bb7c1d0f50614cb8f648
Victor Henrique de Camargo Nestali <victornestali@gmail.com>
2024-07-10 11:25:36 +0200

    Creating new component Button

--- src/pages/MoviePreview/index.jsx
+++ src/pages/MoviePreview/index.jsx
@@ -1,11 +1,12 @@
 import { Container } from "./styles";
+import { Button } from "../../components/Button";
 
 export function MoviePreview() {
   return (
     <Container>
       <h1>Hello World</h1>  
       <span>Victor Nestali</span>
-      <button>Pressione aqui</button>
+      <Button />
     </Container>
   )
 }
