157cad807b015269437bfaab9b86b35c0ebc5c75
Victor Henrique de Camargo Nestali <victornestali@gmail.com>
2024-07-19 17:38:40 +0200

    Creating style for Profile in Header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,19 @@
-import { Container } from "./styles";
+import { Container, Profile } from "./styles";
 
 export function Header() {
   return (
     <Container>
-      
+       <Profile>
+          <div>
+            <strong>Victor Nestali</strong>
+            <button type="button">Sair</button>
+          </div>
+
+          <img 
+            src="https://github.com/devnestali.png" 
+            alt="Foto do usuário" 
+          />
+       </Profile>
     </Container>
   )
 }
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -10,9 +10,40 @@ export const Container = styled.header`
 
   display: flex;
   justify-content: space-between;
-  align-items: center;
   
   padding: 0 6.4rem;
+`;
 
-  background: red;
+export const Profile = styled.div`
+  display: flex;
+  align-items: center;
+
+  > img {
+    width: 6.4rem;
+    height: 6.4rem;
+    border-radius: 50%;
+  }
+
+  > div {
+    display: flex;
+    flex-direction: column;
+    margin-right: 1.6rem;
+    line-height: 2.4rem;
+
+    > strong {
+      color: ${({ theme }) => theme.COLORS.GRAY_100};
+    }
+
+    > button {
+      border: none;
+      outline: none;
+
+      background: transparent;
+      color: ${({ theme }) => theme.COLORS.GRAY_500};
+      text-align: end;
+
+      cursor: pointer;
+    }
+  }
+  
 `;
