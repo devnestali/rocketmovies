0c9ed9ca21e84a51ff03ab87d4c0c9769741de01
Victor Henrique de Camargo Nestali <victornestali@gmail.com>
2024-08-19 15:34:32 +0200

    Creating new page SignIn and Setting default configuration

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,13 @@ import { ThemeProvider } from 'styled-components';
 import theme from './styles/theme';
 import GlobalStyle from './styles/global'; 
 
-import { CreateMovie } from './pages/CreateMovie';
+import { SignIn } from './pages/SignIn';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <CreateMovie />
+      <SignIn />
     </ThemeProvider>
   </React.StrictMode>
 )
