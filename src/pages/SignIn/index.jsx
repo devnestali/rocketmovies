f1448c7d9fb52742362f17c4f88c09bf7e29f05c
Victor Henrique de Camargo Nestali <victornestali@gmail.com>
2024-08-19 16:03:32 +0200

    Removing imports not used in SignIn

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -3,7 +3,6 @@ import { FiMail, FiLock } from "react-icons/fi";
 
 import { Input } from "../../components/Input";
 import { Button } from "../../components/Button";
-import { ButtonText } from "../../components/ButtonText";
 
 export function SignIn() {
   return (
