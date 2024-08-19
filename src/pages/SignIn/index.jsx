d5e55045db00d17a57194d82039f1bd9a4213f4f
Victor Henrique de Camargo Nestali <victornestali@gmail.com>
2024-08-19 16:01:38 +0200

    Defining styles for Form in SignIn

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,9 +1,37 @@
-import { Container } from "./styles";
+import { Container, Form } from "./styles";
+import { FiMail, FiLock } from "react-icons/fi";
+
+import { Input } from "../../components/Input";
+import { Button } from "../../components/Button";
+import { ButtonText } from "../../components/ButtonText";
 
 export function SignIn() {
   return (
     <Container>
+      <Form>
+        <h1>RocketMovies</h1>
+        <p>Aplicação para acompanhar tudo que assistir.</p>
+
+        <h2>Faça seu login</h2>
+
+        <Input 
+          placeholder="E-mail"
+          type="text"
+          icon={FiMail}
+        />
+        
+        <Input 
+          placeholder="Senha"
+          type="password"
+          icon={FiLock}
+        />
+
+        <Button title="Entrar" />
+        <a href="#">
+          Criar conta
+        </a>
       
+      </Form>
     </Container>
   )
 }
--- src/pages/SignIn/styles.js
+++ src/pages/SignIn/styles.js
@@ -6,3 +6,36 @@ export const Container = styled.div`
   display: flex;
   align-items: stretch;
 `;
+
+export const Form = styled.form`
+  padding: 0 13.4rem;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+
+  text-align: center;
+
+  > h1 {
+    font-size: 4.8rem;
+    color: ${({ theme }) => theme.COLORS.PINK};
+  }
+
+  > p {
+    font-size: 1.4rem;
+    color: ${({ theme }) => theme.COLORS.GRAY_200};
+    margin-bottom: 4.8rem;
+  }
+
+  > h2 {
+    font-size: 2.4rem;
+    margin-bottom: 4.8rem;
+  }
+
+  > a {
+    margin: 4.2rem auto 0;
+    text-align: center;
+    color: ${({ theme }) => theme.COLORS.PINK};
+  }
+`;
