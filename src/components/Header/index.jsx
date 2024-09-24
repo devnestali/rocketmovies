9aa5363cc642dcbe569f4b874ad6c612bce65fc2
Victor Henrique de Camargo Nestali <victornestali@gmail.com>
2024-09-24 11:46:30 +0200

    Creating logout and solving bugs

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,17 +1,27 @@
+import { useAuth } from "../../hooks/auth";
+import { useNavigate } from "react-router-dom";
 import { Input } from "../Input";
 import { Container, Profile } from "./styles";
 
 export function Header() {
+  const { signOut } = useAuth();
+  const navigate = useNavigate();
+
+
+  function handleNavigation() {
+    navigate('/profile');
+  }
+  
   return (
     <Container>
         <a to="/">RocketMovies</a>
 
         <Input placeholder="Pesquisar pelo título" />
           
-        <Profile to="/profile">
+        <Profile onClick={handleNavigation()}>
           <div>
             <strong>Victor Nestali</strong>
-            <button type="button">Sair</button>
+            <button type="button" onClick={signOut}>Sair</button>
           </div>
 
           <img 
