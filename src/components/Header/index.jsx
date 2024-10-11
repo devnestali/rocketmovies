9a5174fe9938705780a540d9dfebeeea212f1d65
Victor Henrique de Camargo Nestali <victornestali@gmail.com>
2024-10-11 16:56:44 +0200

    Rewriting code to header component

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import { Wrapper } from "../Wrapper";
-import { Container } from "./styles";
+import { Container, Logout, Profile } from "./styles";
 
 export function Header({ children }) {
   return (
@@ -10,6 +10,17 @@ export function Header({ children }) {
             <h2>RocketMovies</h2>
           </Link>
           {children}
+          <Profile>
+            <div>
+              <p>Victor Henrique de Camargo Nestali</p>
+              <Logout type="button">
+                Sair
+              </Logout>
+            </div>
+            <Link to="/profile">
+              <img src="https://github.com/devnestali.png" alt="Foto do usuário" />
+            </Link>
+          </Profile>
         </Wrapper>
     </Container>
   )
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -23,3 +23,46 @@ export const Container = styled.header`
     }
   }
 `;
+
+export const Profile = styled.div`
+  max-width: 20rem;
+
+  display: flex;
+  align-items: center;
+  gap: .9rem;
+
+  > div {
+    flex-grow: 1;
+
+    p {
+      font-size: 1.4rem;
+      font-weight: 700;
+
+      max-width: 15rem;
+      word-wrap: normal;
+      overflow: hidden;
+      white-space: nowrap;
+      text-overflow: ellipsis;
+    }
+  }
+
+  img {
+    width: 6.4rem;
+    height: 6.4rem;
+
+    border-radius: 50%;
+    border: 2px solid ${({ theme }) => theme.COLORS.GRAY_900};
+  }
+`;
+
+export const Logout = styled.button`
+  display: block;
+  width: 100%;
+
+  border: none;
+  font-weight: 200;
+  background-color: transparent;
+
+  text-align: right;
+  
+`;
