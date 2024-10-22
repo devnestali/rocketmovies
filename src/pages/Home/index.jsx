07a0a4a7a1562b61c735ba90d299ac6f1888aba9
Victor Henrique de Camargo Nestali <victornestali@gmail.com>
2024-10-22 15:16:15 +0200

    Handling viewing of film note on page Home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -5,13 +5,46 @@ import { FiSearch, FiPlus } from "react-icons/fi";
 import { Wrapper } from "../../components/Wrapper";
 import { Note } from "../../components/Note";
 
+import { api } from "../../services/api";
+
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 export function Home() {
+  const [search, setSearch] = useState("");
+  const [notes, setNotes] = useState([]);
+
+  const navigate = useNavigate();
+
+  function handleShowDetails(id) {
+    navigate(`/details/${id}`);
+  }
+  
+  
+  
+  useEffect(() => {
+    async function fetchNotes() {
+      try {
+        const response = await api.get(`/notes?title=${search}`);
+        setNotes(response.data);
+
+      } catch (error) {
+        if(error.message) {
+          alert(error.response.data.message);
+        } else {
+          alert("Não foi possível carregar as notas. Por favor, tente mais tarde.");
+        }
+      }
+    }
+    fetchNotes();
+  }, [search])
   return (
     <Container>
       <Header>
         <Input 
           className="search toDesktop"
           placeholder="Pesquisar pelo título"
+          onChange={e => setSearch(e.target.value)}
         />
         <button className="mobileSearch"><FiSearch /></button>
       </Header>
@@ -24,14 +57,15 @@ export function Home() {
             </ButtonAdd>
           </MainTop>
           <Notes>
-            <Note />
-            <Note />
-            <Note />
-            <Note />
-            <Note />
-            <Note />
-            <Note />
-            <Note />
+            {
+              notes.map((note) => (
+                <Note 
+                  key={String(note.id)}
+                  data={note}
+                  onClick={() => handleShowDetails(note.id)}
+                />
+              ))
+            }
           </Notes>
         </Wrapper>
       </main>
