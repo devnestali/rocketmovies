a295c059c9e967ef870b13f44bebcefa1d376616
Victor Henrique de Camargo Nestali <victornestali@gmail.com>
2024-10-24 11:41:54 +0200

    Handling page edit

--- src/pages/Edit/index.jsx
+++ src/pages/Edit/index.jsx
@@ -15,27 +15,94 @@ import { useEffect, useState } from 'react';
 import { api } from '../../services/api';
 
 export function Edit() {
-    const [data, setData] = useState({});
-    
+    const [title, setTitle] = useState("");
+    const [rating, setRating] = useState("");
+    const [description, setDescription] = useState("");
+    const [newTag, setNewTag] = useState("");
+    const [tags, setTags] = useState([]);
+
     const params = useParams();
     const navigate = useNavigate();
+
+    function inputValidation() {
+        if (!title) {
+            alert("É preciso adicionar um título para cadastrar um filme.")
+            return false;
+        };
+
+        const validRating = rating >= 0 && rating <= 5 && rating !== "";
+
+        if (!validRating) {
+            alert("É preciso informar uma nota entre 0 e 5 para cadastrar um filme.");
+            return false;
+        }
+
+        if (newTag) {
+            alert("Um marcador foi preenchido, porem não foi adicionado. Adicione-o ou deixe o campo vazio.")
+            return false;
+        }
+        return true;
+    }
+
+    function handleAddNewTag() {
+        const tagAlreadyExists = tags.includes(newTag);
+
+        if (tagAlreadyExists) {
+            return alert("Esta tag já foi adicionada!");
+        }
+
+        if (newTag != "") {
+            setTags(previousTags => [...previousTags, newTag]);
+            setNewTag("");
+        }
+    }
+
+    function handleDeleteTag(deleted) {
+        const tagsWithoutDeletedTag = tags.filter(tag => tag !== deleted);
+        setTags(tagsWithoutDeletedTag);
+    }
+
+    async function handleSaveChanges() {
+        const passedInAllValidations = inputValidation();
+
+        if(passedInAllValidations) {
+            try {
+                await api.put(`/notes/${params.id}`, { title, rating, description, tags});
+                alert("Filme editado com sucesso!");
+                navigate("/");
+            } catch (error) {
+                if(error.response) {
+                    alert(error.response.data.message);
+                } else {
+                    alert("Não foi possível realizar o cadastro do filme. Por favor, tente mais tarde.");
+                }
+            }
+        }
+    }
 
     function handleDiscardChanges() {
         const userConfirmation = confirm("Todas as alterações serão perdidas, deseja continuar?");
 
-        if(userConfirmation) {
+        if (userConfirmation) {
             navigate("/");
         }
     }
-
 
     useEffect(() => {
         async function fetchData() {
             try {
                 const response = await api.get(`/notes/${params.id}`);
-                setData(response.data)
+                const { data } = response;
+
+                setTitle(data.title);
+                setRating(data.rating);
+                setDescription(data.description);
+                
+                const tagNameOnly = data.tags.map(tag => tag.name);
+                
+                setTags(tagNameOnly);
             } catch (error) {
-                if(error.response) {
+                if (error.response) {
                     alert(error.response.data.message);
                 } else {
                     alert("Não foi possível carregar os dados desse filme. Por favor, tente mais tarde.");
@@ -50,55 +117,61 @@ export function Edit() {
             <Header />
             <main>
                 <Wrapper>
-                    <ButtonText 
+                    <ButtonText
                         to="/"
                         icon={FiArrowLeft}
                         title="Voltar"
                     />
                     <h1>Editar Filme</h1>
                     <Inputs>
-                        <Input 
+                        <Input
                             placeholder="Título"
-                            value={data.title}
+                            value={title}
+                            onChange={e => setTitle(e.target.value)}
                         />
-                        <Input 
+                        <Input
                             type='number'
                             placeholder='Sua nota (de 0 a 5)'
                             min='0'
                             max='0'
-                            value={data.rating}
+                            value={rating}
+                            onChange={e => setRating(e.target.value)}
                         />
                     </Inputs>
-                    <Textarea 
+                    <Textarea
                         placeholder='Descrição'
-                        value={data.description}
+                        value={description}
+                        onChange={e => setDescription(e.target.value)}
                     />
                     <h2>Marcadores</h2>
-                    {
-                        data.tags && (
-                            <section className="itemsOfNote">
-                                {
-                                    data.tags.map(tag => (
-                                        <NoteItem 
-                                            key={tag.id}
-                                            value={tag.name}
-                                        />
-                                    ))
-                                }
-                                <NoteItem isnew />      
-                            </section>
+                    <section className="itemsOfNote">
+                        {
+                            tags.map((tag, index) => (
+                                <NoteItem
+                                    key={`${tag}-${index}`}
+                                    value={tag}
+                                    onClick={() => handleDeleteTag(tag)}
+                                />
+                            ))
+                        }
+                        <NoteItem
+                            isnew
+                            placeholder="Nova tag"
+                            value={newTag}
+                            onChange={e => setNewTag(e.target.value)}
+                            onClick={handleAddNewTag}
+                        />
+                    </section>
 
-                        )
-                    }
-                    
                     <Buttons>
-                        <Button 
+                        <Button
                             title="Descartar alterações"
                             highlight={false}
                             onClick={handleDiscardChanges}
                         />
-                        <Button 
+                        <Button
                             title="Salvar alterações"
+                            onClick={handleSaveChanges}
                         />
                     </Buttons>
                 </Wrapper>
